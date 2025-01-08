0d01623d5fd239c739773d4e4f4da488fae3f8d2
Aboloooo <abolfazle0915501@gmail.com>
2025-01-08 15:17:22 +0100

    rthj

--- EnglishVersion/Home.php
+++ EnglishVersion/Home.php
@@ -25,29 +25,67 @@ include_once("../phpLibrary/MyLibrary.php");
     NavigationBarE("Home");
     ?>
 
-    <div class="SignInUpBtn">
-        <a href="Login.php" class="LoginBtn"> <?php
-                                                if ($_SESSION["user"]) {
-                                                ?>
-                <!-- here we convert the link into a btn for loging out -->
-                <form action="" method="POST">
-                    <input type="submit" value="<?= $arrayOfStrings["Logout"] ?>" name="Logout">
-                </form>
-            <?php   //function of the logout btn
-                                                    if (isset($_POST["Logout"])) {
-                                                        session_unset();
-                                                        session_destroy();
-                                                        header("Refresh:0");  //this refreash doesnt function
-                                                    }
-                                                } else {
-                                                    print($arrayOfStrings["Login"]);
-                                                }
-            ?>
-        </a>
-    </div>
+    <!-- <div class="SignInUpBtn">
+        <div class="loginBtnDesign">
+            <a href="Login.php" class="LoginBtn"> <?php
+                                                    /*  if ($_SESSION["user"]) { */
+                                                    ?>
+                     here we convert the link into a btn for loging out -->
+    <!--  <form action="" method="POST">
+                        <input type="submit" value="<?= $arrayOfStrings["Logout"] ?>" name="Logout">
+                    </form> -->
+    <!--  <?php   //function of the logout btn
+            /*    if (isset($_POST["Logout"])) {
+                                                            session_unset();
+                                                            session_destroy();
+                                                            header("Refresh:0");
+                                                        }
+                                                    } else {
+                                                        print($arrayOfStrings["Login"]);
+                                                    } */
+            ?> -->
+    </a>
+    <!--  </div>
+        <div class="SignUpBtnDesign">
+            <a href="SignUp.php"><?= $arrayOfStrings["SignUp"] ?></a>
+        </div> -->
 
-    <div>
-        <a href="SignUp.php"><?= $arrayOfStrings["SignUp"] ?></a>
+    </div>
+    <!-- sign in/up btn -->
+    <div class="signInOutBtn">
+
+
+        <?php
+        if ($_SESSION["user"]) {
+        ?>
+            <form method="post">
+                <input type="submit" value="Logout" name="Logout">
+            </form>
+
+        <?php
+
+        } else {
+        ?>
+            <form method="post">
+                <input type="submit" value="Sign up" name="SignUp">
+                <input type="submit" value="Login" name="Login">
+            </form>
+
+        <?php
+        }
+        if (isset($_POST["Login"])) {
+            header("Location: Login.php");
+            }
+        if (isset($_POST["SignUp"])) {
+            header("Location: SignUp.php");
+        }
+        if (isset($_POST["Logout"])) {
+            session_unset();
+            session_destroy();
+            header("Refresh:0");
+        }
+        ?>
+
     </div>
 
 </body>
