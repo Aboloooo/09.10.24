c5445e5394153f03765a7548f602d13b5112d246
abolo <abolfazle0915501@gmail.com>
2025-04-09 20:51:03 +0200

    tzht

--- EnglishVersion/CheckedOut.php
+++ EnglishVersion/CheckedOut.php
@@ -25,39 +25,78 @@ include_once("../phpLibrary/MyLibrary.php");
             <input type="submit" value="<?= $arrayOfStrings["Go"] ?>">
         </form>
         <?php
-        $sqlOrderInfor = $connection->prepare('select * from orders');
+        $sqlOrderInfor = $connection->prepare('SELECT * FROM orders');
+        $sqlOrderContent = $connection->prepare('SELECT productsID FROM orderContent WHERE orderID = ?');
+
         if ($connection->connect_error) {
             die("Connection failed: " . $connection->connect_error);
         } else {
             $sqlOrderInfor->execute();
             $result = $sqlOrderInfor->get_result();
+
             while ($row = $result->fetch_assoc()) {
                 $orderID = $row['orderID'];
+                $userID = $row['userID'];
+                $actionDate = $row['actionDate'];
+                $actionTime = $row['actionTime'];
+
+                // Get products for this order
+                $sqlOrderContent->bind_param('i', $orderID);
+                $sqlOrderContent->execute();
+                $productIDsforEachOrder = $sqlOrderContent->get_result();
         ?>
-                <!-- create table for each order row -->
-                
+                <div>
+                    <h3>An order placed on <?= $actionDate ?> at <?= $actionTime ?></h3>
+                </div>
+                <!-- Create table for each order row -->
                 <div class="tableOfOrder">
-                <h2>order ID <?= $orderID ?></h2>
+                    <h2>Order ID <?= $orderID ?></h2>
                     <div class="imgProducts">
-                        <img src="../img/Men/1/1.1.PNG" alt="">
-                        <div></div>
+                        <?php
+                        while ($productIDs = $productIDsforEachOrder->fetch_assoc()) {
+                            $productID = $productIDs['productsID'];
+
+                            // Get product info
+                            $product = $connection->prepare('SELECT * FROM products WHERE productsID = ?');
+                            $product->bind_param('i', $productID);
+                            $product->execute();
+                            $productInfo = $product->get_result();
+                            $productInformation = $productInfo->fetch_assoc();
+
+                            if ($productInformation) {
+                                $productName = $productInformation['productName'];
+                                $productPrice = $productInformation['Price'];
+                                $productImg = $productInformation['img'];
+                        ?>
+                                <table class="tableOfOrders">
+                                    <tr>
+                                        <th></th>
+                                        <th>Name</th>
+                                        <th>Price</th>
+                                    </tr>
+                                    <tr>
+                                        <td><img src="<?= $productImg ?>" width="100px" height="110px"></td>
+                                        <td><?= $productName ?></td>
+                                        <td><?= $productPrice ?></td>
+                                    </tr>
+                                </table>
                     </div>
-                    <div>
-                        <h3>time</h3>
-                        <h3>date</h3>
-                    </div>
+            <?php
+                            }
+                        }
+            ?>
                 </div>
 
-
-        <?php
-
+    </div>
+<?php
             }
         }
-        ?>
+?>
 
 
 
-    </div>
+
+</div>
 </body>
 
 </html>
